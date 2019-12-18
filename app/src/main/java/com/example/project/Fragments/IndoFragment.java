3b9984eebaecb54fb8544966f4fd03517d6f841c
YonathanTobias <yonathantobias@gmail.com>
2019-12-18 15:33:01 +0700

    Menambah asset

--- app/src/main/java/com/example/project/Fragments/IndoFragment.java
+++ app/src/main/java/com/example/project/Fragments/IndoFragment.java
@@ -3,6 +3,7 @@ package com.example.project.Fragments;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -26,7 +27,24 @@ public class IndoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_indo, container, false);
+        View view = inflater.inflate(R.layout.fragment_indo, container, false);
+
+        CardView cardView1 = (CardView) view.findViewById(R.id.cardviewindo4);
+        cardView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                BaliFragment f = new BaliFragment();
+                f.setArguments(bundle);
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, f)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        CardView cardView2 = (CardView) view.findViewById(R.id.cardviewindo3);
+        return view;
     }
 
 }
